f38d200e17273e2925be8135f094d280b544dade
JunLife <alswns9288@gmail.com>
2022-05-03 23:11:37 +0900

    [ADD] 상수, 문자 더하기

--- querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -3,6 +3,7 @@ package study.querydsl;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -406,6 +407,35 @@ public class QuerydslBasicTest {
                         .when(member.age.between(21, 30)).then("21살 ~ 30살")
                         .otherwise("기타")
                 )
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    /**
+     * 상수 표시하기
+     */
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory.select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    /**
+     * 문자 더하기
+     */
+    @Test
+    public void concat() {
+        // {username}_{age}
+        List<String> result = queryFactory.select(member.username.concat("_").concat(member.age.stringValue()))
                 .from(member)
                 .fetch();
 
